34d00985a803c7647dc1291800c7b0de9b58ef03
sunflo-h <sunflo0524@gmail.com>
2022-03-18 15:17:08 +0900

    data.js 값입력 완료 및 cardlist 출력 완료

--- src/component/Cardlist.js
+++ src/component/Cardlist.js
@@ -5,38 +5,21 @@ function CardList(props) {
     <div className={styles.cardContainer}>
       <h1>BEST</h1>
       <ul className={styles.cardList}>
-        <li>
-          <div className={styles.imageContainer}>
-            <img src={props.tshirt[0].src}></img>
-          </div>
-          <div className={styles.textContainer}>
-            <p>오렌지 티셔츠</p>
-            <p>15000원</p>
-          </div>
-        </li>
-        <li>
-          <div className={styles.imageContainer}>
-            <img src={props.tshirt[1].src}></img>
-          </div>
-          <div className={styles.textContainer}>
-            <p>야자수 티셔츠</p>
-            <p>15000원</p>
-          </div>
-        </li>
-        <li>
-          <div className={styles.imageContainer}>
-            <img src={props.tshirt[2].src}></img>
-          </div>
-          <div className={styles.textContainer}>
-            <p>야자수 티셔츠</p>
-            <p>15000원</p>
-          </div>
-        </li>
-        <li>c</li>
-        <li>d</li>
-        <li>f</li>
-        <li>g</li>
-        <li>h</li>
+        {
+          props.tshirt.map((item,i) => {
+            return (
+              <li key = {i}>
+                <div className={styles.imageContainer}>
+                  <img src={props.tshirt[i].src}></img>
+                </div>
+                <div className={styles.textContainer}>
+                  <p>{props.tshirt[i].name}</p>
+                  <p>{props.tshirt[i].price}원</p>
+                </div>
+              </li>
+            )
+          })
+        } 
       </ul>
     </div>
 
