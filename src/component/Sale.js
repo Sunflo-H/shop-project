dbedb397f53b71f5a02a19e7a5eb67d98926bab8
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2022-03-23 18:20:45 +0900

    Sale 컴포넌트 완성

--- src/component/Sale.js
+++ src/component/Sale.js
@@ -1,19 +1,28 @@
 import styles from '../css/Sale.module.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBolt } from '@fortawesome/free-solid-svg-icons';
-
+import { faBolt,faArrowLeftLong, faArrowRightLong } from '@fortawesome/free-solid-svg-icons';
+import {Link} from 'react-router-dom';
 function Sale() {
     return (
         <div className={styles.saleContainer}>
-            <div className={styles.textContainer}>
-                <div>
-                    <p><FontAwesomeIcon icon={faBolt} className={styles.boltIcon} /></p>
-                    <p className={styles.text}>SALE IS ON!</p>
-                    <p className={styles.highLight}>25% OFF</p>
+            <div className={styles.containerTop}>
+                <div className={styles.textContainer}>
+                    <div>
+                        <p><FontAwesomeIcon icon={faBolt} className={styles.boltIcon} /></p>
+                        <p className={styles.text}>SALE IS ON!</p>
+                        <p className={styles.highLight}>25% OFF</p>
+                    </div>
+                </div>
+                <div className={styles.imageContainer}>
+                    <img src={'assets/img/sale.jpg'}></img>
                 </div>
             </div>
-            <div className={styles.imageContainer}>
-                <img src={'assets/img/sale.jpg'}></img>
+            <div className={styles.containerBottom}>
+                <div>
+                    <FontAwesomeIcon icon={faArrowRightLong} className={styles.rightArrow}/>
+                    <span><Link to="/woman">GET 25% OFF WOMEN'S T-SHIRTS</Link></span>
+                    <FontAwesomeIcon icon={faArrowLeftLong} className={styles.leftArrow}/>
+                </div>
             </div>
         </div>
     )
