8f84f78e7d0acd81e25904749ca3cbb294b0bd17
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2022-03-24 22:57:26 +0900

    공부

--- src/study/Study.js
+++ src/study/Study.js
@@ -1,16 +1,31 @@
-import React from 'react';
-import Hello from './Hello';
-import Wrapper from './Wrapper';
+import React, {useRef} from 'react';
+
+import UserList from './UserList';
 
 function App() {
-  return (
-    //   <>
-    <Wrapper>
-      <Hello name="react" color="red"/>
-      <Hello color="pink"/>
-    </Wrapper>
-    // </>
-  );
+  const users = [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ];
+  const nextId = useRef(4);
+  console.log(nextId);
+  const onCreate = () => {
+    nextId.current += 1;
+  }
+  return <UserList users={users} />;
 }
 
 export default App;
