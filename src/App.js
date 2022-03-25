fba81fe5c44caf3ac9f1e6d87331d20707f96c53
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2022-03-25 18:38:07 +0900

    헤더 컴포넌트로 만듬

--- src/App.js
+++ src/App.js
@@ -4,10 +4,9 @@ import './css/reset.css';
 import styles from './App.module.css';
 import data from './data.js';
 
+import Header from './component/Header';
 import Jumbotron from './component/MainJumbotron';
 import Card from './component/CardList';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
 import Test from './component/Test';
 import Test2 from './component/Test2';
 import Test3 from './component/Test3';
@@ -21,36 +20,22 @@ function App() {
   function getNewTshirts() {
     let totalCount = tshirts.length;
     const COUNT = 8;
-    let newTshirts = tshirts.filter((tshirt,i) => (totalCount-i) <= (totalCount - COUNT ));
+    let newTshirts = tshirts.filter((tshirt, i) => (totalCount - i) <= (totalCount - COUNT));
     return newTshirts;
   }
   return (
     <div className={styles.App}>
-      {/* 헤더 */}
-      <header>
-        <div className={styles.logo}><Link to={'/'}>T SHOP</Link></div>
-        <div className={styles.menu}>
-          <ul>
-            <li><Link to={'/new'}>New</Link></li>
-            <li><Link to={'/woman'}>Woman</Link></li>
-            <li><Link to={'/man'}>Man</Link></li>
-          </ul>
-        </div>
-        <div className={styles.login}>
-          <FontAwesomeIcon icon={faCircleUser} className={styles.userIcon} />
-          <span>Log In</span>
-        </div>
-      </header>
 
       <Route exact path={"/"}>
-      {/* 메인 */}
+        <Header />
         <Jumbotron></Jumbotron>
         <Card tshirts={getNewTshirts()}></Card>
         <Sale></Sale>
-      
+
       </Route>
       <Route path="/:category">
-          <SubPage tshirts={tshirts}></SubPage>
+        <Header />
+        <SubPage tshirts={tshirts}></SubPage>
       </Route>
       {/* 푸터 */}
       {/* <Footer></Footer>   */}
---
+++ src/component/Header.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from '../css/Header.module.css';
+import {Link} from 'react-router-dom';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleUser } from '@fortawesome/free-solid-svg-icons';
+const Header = () => {
+    return (
+        <header>
+            <div className={styles.logo}><Link to={'/'}>T SHOP</Link></div>
+            <div className={styles.menu}>
+                <ul>
+                    <li><Link to={'/new'}>New</Link></li>
+                    <li><Link to={'/woman'}>Woman</Link></li>
+                    <li><Link to={'/man'}>Man</Link></li>
+                </ul>
+            </div>
+            <div className={styles.login}>
+                <FontAwesomeIcon icon={faCircleUser} className={styles.userIcon} />
+                <span>Log In</span>
+            </div>
+        </header>
+    )
+}
+
+export default Header;
