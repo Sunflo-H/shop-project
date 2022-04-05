f1eb153568793b23b494d8a4765a99cfcbd23411
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2022-04-05 20:32:39 +0900

    reducer 공부

--- src/index.js
+++ src/index.js
@@ -6,13 +6,14 @@ import reportWebVitals from './reportWebVitals';
 import {HashRouter} from 'react-router-dom';
 import Test3 from './component/Test3';
 import Study from './study/Study';
+import Counter from './study/Counter';
 
 ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
-      <App />
-      {/* <Test3></Test3> */}
-      {/* <Study></Study> */}
+      {/* <App /> */}
+      <Study />
+      {/* <Counter /> */}
     </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
