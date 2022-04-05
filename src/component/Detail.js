febdf7f13c50cd36911b3000039de2fe7e083aa0
Sunflo-H <70611956+Sunflo-H@users.noreply.github.com>
2022-04-06 03:42:40 +0900

    history, name 부분만 컴포넌트화 시킴

--- src/component/Detail.js
+++ src/component/Detail.js
@@ -8,15 +8,29 @@ import { faBasketShopping } from '@fortawesome/free-solid-svg-icons';
 function capitalize(str) {
     return str.charAt(0).toUpperCase() + str.slice(1);
 }
-const History = () => {
-    // return (
-    //     <div className={styles.historyContainer}>
-    //                 <Link to={'/'}>Home</Link>
-    //                 <span>&nbsp;&gt;&nbsp;</span>
-    //                 <Link to={`/${category}`}>{capitalize(category)}</Link>
-    //             </div>
-    // )
+const History = ({ category }) => {
+    return (
+        <div className={styles.historyContainer}>
+            <Link to={'/'}>Home</Link>
+            <span>&nbsp;&gt;&nbsp;</span>
+            <Link to={`/${category}`}>{capitalize(category)}</Link>
+        </div>
+    )
 }
+
+const Name = ({tshirt}) => {
+    return (
+        <div className={styles.nameContainer}>
+            {
+                tshirt.sale === true ? <div className={styles.sale}>SALE</div> : null
+            }
+            <div className={styles.name}>
+                <span>{tshirt.name}</span>
+            </div>
+        </div>
+    )
+}
+
 const Detail = ({ tshirts }) => {
     const { category, id } = useParams();
     const [tshirt, setTshirt] = useState(() => getTshirts());
@@ -41,20 +55,8 @@ const Detail = ({ tshirts }) => {
     return (
         <div className={styles.detailContainer}>
             <div className={styles.wrapper}>
-                <div className={styles.historyContainer}>
-                    <Link to={'/'}>Home</Link>
-                    <span>&nbsp;&gt;&nbsp;</span>
-                    <Link to={`/${category}`}>{capitalize(category)}</Link>
-                </div>
-
-                <div className={styles.nameContainer}>
-                    {
-                        tshirt.sale === true ? <div className={styles.sale}>SALE</div> : null
-                    }
-                    <div className={styles.name}>
-                        <span>{tshirt.name}</span>
-                    </div>
-                </div>
+                <History category={category} />
+                <Name tshirt={tshirt}/>
 
                 <div className={styles.infoContainer}>
                     <div className={styles.imageContainer}>
@@ -87,7 +89,7 @@ const Detail = ({ tshirts }) => {
                         </table>
 
                         <div className={styles.cartContainer}>
-                            <div className={styles.select} onChange={onChangeSelect} value={selected}> 
+                            <div className={styles.select} onChange={onChangeSelect} value={selected}>
                                 <select>
                                     <option defaultValue="옵션선택">옵션 선택</option>
                                     <option defaultValue="free" >free</option>
